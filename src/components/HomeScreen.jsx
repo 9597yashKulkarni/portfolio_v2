8c4dcdc9ac25cc80b8c3c824a46f5cf995e1b24c
Yash Kulkarni <yash.kulkarni9597@gmail.com>
2024-06-03 00:03:35 +0530

    Initial commit for Portfolio website

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomeScreen = () => {
+        return (
+                <div className="home-screen">
+                        <div className="container">
+                                <div className="row">
+                                        <div className="col-12">
+                                                Image over here and Hello text 
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        )
+};
+
+export default HomeScreen;
