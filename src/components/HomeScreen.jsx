b6a9b3928144f7f7e91cc07e2eec77e13e9c0ac7
Yash Kulkarni <yash.kulkarni9597@gmail.com>
2024-06-04 08:56:55 +0530

    included bootstrap and created sample header

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 
 const HomeScreen = () => {
-        return (
-                <div className="home-screen">
-                        <div className="container">
-                                <div className="row">
-                                        <div className="col-12">
-                                                Image over here and Hello text 
-                                        </div>
-                                </div>
-                        </div>
-                </div>
-        )
+	return (
+		<div className="home-screen h-100">
+			<div className="container">
+				<div className="p-md-5 mt-5 bg-light">
+					<div className="row">
+						<div className="col-12 col-md-6">
+							<div className="welcome-text d-flex justify-content-center">
+								<p className="lead">
+									Hi, I'm 
+									<h1>Yash Kulkarni</h1> 
+									- A Passionate Web Developer
+								</p>
+							</div> 
+						</div>
+						<div className="col-12 col-md-6">
+							<img src="" alt="" />		
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 };
 
 export default HomeScreen;
