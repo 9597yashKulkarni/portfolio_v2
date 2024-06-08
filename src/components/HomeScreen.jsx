8bb809877d4cd863f1f621d33050b3923cd37b24
Yash Kulkarni <yash.kulkarni9597@gmail.com>
2024-06-09 00:04:57 +0530

    Download resume and skills section

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import web_pic from "../assets/portfolio_web_pic-removebg.png";
 import '../sass/_homeScreen.scss';
+import copyTexts from '../copyText.json';
 
 const HomeScreen = () => {
+	const onDownloadClick = () => {
+        const pdfUrl = "Yash'sResume.pdf";
+        const link = document.createElement("a");
+        link.href = pdfUrl;
+        link.download = "Yash's Resume.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+	const skills = [
+		'React js',
+		'JavaScript',
+		'Redux',
+		'Hooks',
+		'AEM',
+		'HTML',
+		'CSS',
+		'Bootstrap',
+		'Java',
+		'Spring boot',
+		'JPA',
+		'REST',
+		'Python',
+		'Django',
+		'Node js',
+		'Express',
+		'SQL',
+		'NoSQL',
+		'MongoDb',
+		'React Native',
+		'Django Rest Framework',
+		'JIRA',
+		'BitBucket',
+		'Git',
+		'Github',
+		'Perforce',
+		'Bamboo',
+		'Jenkins'
+	]
 	return (
 		<div className="home-screen">
 			<div className="header-section bg-light">
@@ -23,15 +64,84 @@ const HomeScreen = () => {
 							<div className="row w-100 mt-4">
 								<div className="col-12 col-md-10">
 									<div className="d-flex justify-content-center justify-content-md-end">
-										<button className="btn btn-primary">Download Resume</button>
+										<button className="btn btn-primary" onClick={onDownloadClick}>Download Resume</button>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div className="col-12 col-md-6">
+						<div className="col-12 col-md-6 mt-md-4">
 							<div className="d-flex align-items-center">
 								<img className="my-pic" src={web_pic} alt="" />
-							</div>		
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="container mt-4 details-section">
+				<div className="row">
+					<div className="col-12 col-md-6 about-me">
+						<div className="row">
+							<div className="col-12">
+								<h4>
+									{copyTexts.homeScreen.aboutMeHeader}
+								</h4>
+							</div>
+						</div>
+						<div className="row mt-2">
+							<div className="col-12 offset-md-1 col-md-11">
+								<p className="lead">
+									{copyTexts.homeScreen.aboutMeLeadPara}
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="col-12 col-md-6 skills">
+						<div className="row">
+							<div className="col-12">
+								<h4>
+									{copyTexts.homeScreen.skills}
+								</h4>
+							</div>
+						</div>
+						<div className="row mt-2">
+							<div className="col-12 offset-md-1 col-md-11">
+								{skills.map((skill) => {
+									return (
+										<span className="badge skill-badge">
+											{skill}
+										</span>
+									)
+								})}	
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="container experience-section mt-5">
+				<div className="row">
+					<div className="col-12">
+						<h4>{copyTexts.homeScreen.experience}</h4>
+					</div>
+				</div>
+				<div class="row mt-4">
+					<div class="col-12 col-md-6">
+						<div className="experience cursor-pointer">
+							<h3 className="mb-0">
+								Amdocs
+							</h3>
+							<small class="text-muted">2021-Today</small>	
+							<p className="lead mt-3 exp-desc">{copyTexts.homeScreen.amdocs_experience}</p>
+						</div>
+					</div>
+				</div>
+				<div class="row mt-4">
+					<div class="col-12 col-md-6 offset-md-6">
+						<div className="experience cursor-pointer">
+							<h3 className="mb-0">
+								Infosys
+							</h3>
+							<small class="text-muted">2019-2021</small>	
+							<p className="lead mt-3 exp-desc">{copyTexts.homeScreen.infy_experience}</p>
 						</div>
 					</div>
 				</div>
